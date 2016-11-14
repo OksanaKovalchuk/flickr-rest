b9e4ad2b9322d1766f2be13d0178556d13bfd4a5
OksanaKovalchuk <kovalchuk.oksana17@gmail.com>
2016-11-14 03:41:40 +0200

    added index and parser

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -13,7 +12,19 @@ class PhotoController  extends Controller
     public function index()
     {
         try {
+            $apiKey = 'cd51c35deb0b194c8c3ccbf6e18954c5';
+            $method = 'flickr.photos.getRecent';
+            $url = "https://api.flickr.com/services/rest/?method=".$method."&api_key=".$apiKey;
 
+            $new = curl_init();
+            curl_setopt($new, CURLOPT_URL, $url);
+            curl_setopt($new, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($new,CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+
+            $result =(curl_exec($new));
+            $a = $this->namespacedXMLToArray($result);
+            Log::info($a);
+            curl_close($new);
 
         } catch (\Exception $e) {
             return response()->json([
@@ -23,14 +34,43 @@ class PhotoController  extends Controller
         }
 
         return response()->json([
-            'photos' => "photos will be here"
+            'photos' => $a
         ]);
     }
-    public function photo(Request $request)
-    {
+    public function photo(Request $request){
+          }
+    public function  getBySize(){
     }
 
-    public function  getBySize(){
+    function namespacedXMLToArray($xml)
+    {
+        function removeNamespaceFromXML( $xml )
+        {
+            // Because I know all of the the namespaces that will possibly appear in
+            // in the XML string I can just hard code them and check for
+            // them to remove them
+            $toRemove = ['rap', 'turss', 'crim', 'cred', 'j', 'rap-code', 'evic'];
+            // This is part of a regex I will use to remove the namespace declaration from string
+            $nameSpaceDefRegEx = '(\S+)=["\']?((?:.(?!["\']?\s+(?:\S+)=|[>"\']))+.)["\']?';
 
+            // Cycle through each namespace and remove it from the XML string
+            foreach( $toRemove as $remove ) {
+                // First remove the namespace from the opening of the tag
+                $xml = str_replace('<' . $remove . ':', '<', $xml);
+                // Now remove the namespace from the closing of the tag
+                $xml = str_replace('</' . $remove . ':', '</', $xml);
+                // This XML uses the name space with CommentText, so remove that too
+                $xml = str_replace($remove . ':commentText', 'commentText', $xml);
+                // Complete the pattern for RegEx to remove this namespace declaration
+                $pattern = "/xmlns:{$remove}{$nameSpaceDefRegEx}/";
+                // Remove the actual namespace declaration using the Pattern
+                $xml = preg_replace($pattern, '', $xml, 1);
+            }
+
+            // Return sanitized and cleaned up XML with no namespaces
+            return $xml;
+        }
+        // One function to both clean the XML string and return an array
+        return json_decode(json_encode(simplexml_load_string(removeNamespaceFromXML($xml))), true);
     }
 }
